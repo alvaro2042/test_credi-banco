c43bde28884a87903a0b16b3e10cc627d791b1fe
alvaro2042 <128509813+alvaro2042@users.noreply.github.com>
2023-10-01 18:08:00 -0500

    Create test_routes.py

---
+++ test/test_routes.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app
+
+class TestRoutes(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_index_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Bienvenido a mi aplicación web en Python", response.data)
+
+if __name__ == '__main__':
+    unittest.main()
